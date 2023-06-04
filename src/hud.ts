b9cf1e80b94cf35dbd90c7a42a5b8b2e0c1bdba5
SamadiPour <samadipoor2@gmail.com>
2023-06-04 14:13:12 +0330

    refactor: copy image with raycast api

--- src/hud.ts
+++ src/hud.ts
@@ -1,4 +1,4 @@
-import {closeMainWindow, showHUD} from "@raycast/api";
+import {Clipboard, closeMainWindow, showHUD} from "@raycast/api";
 import puppeteer from "puppeteer";
 import {v4 as uuidv4} from 'uuid';
 import {execa} from "execa";
@@ -15,11 +15,16 @@ export default async function main() {
         await page.goto('https://www.nytimes.com/');
         const downloadPath = tmpdir() + "/" + uuidv4() + ".png";
         await page.screenshot({path: downloadPath, fullPage: true, type: "png"});
-        await execa(
-            `osascript -e 'tell app "Finder" to set the clipboard to ( POSIX file "${downloadPath}" )'`,
-            [],
-            {shell: true}
-        );
+
+        // with Execa
+        // await execa(
+        //     `osascript -e 'tell app "Finder" to set the clipboard to ( POSIX file "${downloadPath}" )'`,
+        //     [],
+        //     {shell: true}
+        // );
+
+        // with Raycast clipboard
+        await Clipboard.copy({file: downloadPath});
 
         await showHUD('Screenshot copied to your clipboard.');
     } catch (err) {
