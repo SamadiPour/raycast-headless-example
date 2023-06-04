c54cced04e030796513385993a57b55685ce0fa2
SamadiPour <samadipoor2@gmail.com>
2023-06-04 14:31:38 +0330

    style: run "ray lint --fix"

--- src/details.tsx
+++ src/details.tsx
@@ -1,47 +1,46 @@
-import {Detail, List} from "@raycast/api";
-import {useEffect, useState} from "react";
+import { Detail, List } from "@raycast/api";
+import { useEffect, useState } from "react";
 import puppeteer from "puppeteer";
 
 interface State {
-    items?: string[];
-    error?: Error;
+  items?: string[];
+  error?: Error;
 }
 
 export default function main() {
-    const [state, setState] = useState<State>({});
+  const [state, setState] = useState<State>({});
 
-    useEffect(() => {
-        async function fetchNews() {
-            const browser = await puppeteer.launch({headless: true});
-            const page = await browser.newPage();
+  useEffect(() => {
+    async function fetchNews() {
+      const browser = await puppeteer.launch({ headless: true });
+      const page = await browser.newPage();
 
-            try {
-                await page.goto('https://news.ycombinator.com/news');
-                const news = await page.$$eval(
-                    'tr.athing .title > .titleline > a',
-                    links => links.map(link => link.textContent)
-                );
-
-                setState({items: news});
-            } catch (error) {
-                setState({
-                    error: error instanceof Error ? error : new Error("Something went wrong"),
-                });
-            }
-        }
-
-        fetchNews();
-    }, []);
-
-    if (state.error != null) {
-        return <Detail markdown={state.error.message}/>
-    } else {
-        return (
-            <List isLoading={!state.items && !state.error}>
-                {state?.items?.map((item, index) => (
-                    <List.Item key={index} title={item}/>
-                ))}
-            </List>
+      try {
+        await page.goto("https://news.ycombinator.com/news");
+        const news = await page.$$eval("tr.athing .title > .titleline > a", (links) =>
+          links.map((link) => link.textContent)
         );
+
+        setState({ items: news });
+      } catch (error) {
+        setState({
+          error: error instanceof Error ? error : new Error("Something went wrong"),
+        });
+      }
     }
+
+    fetchNews();
+  }, []);
+
+  if (state.error != null) {
+    return <Detail markdown={state.error.message} />;
+  } else {
+    return (
+      <List isLoading={!state.items && !state.error}>
+        {state?.items?.map((item, index) => (
+          <List.Item key={index} title={item} />
+        ))}
+      </List>
+    );
+  }
 }
--- src/hud.ts
+++ src/hud.ts
@@ -1,35 +1,35 @@
-import {Clipboard, closeMainWindow, showHUD} from "@raycast/api";
+import { Clipboard, closeMainWindow, showHUD } from "@raycast/api";
 import puppeteer from "puppeteer";
-import {v4 as uuidv4} from 'uuid';
-import {execa} from "execa";
-import {tmpdir} from "os";
+import { v4 as uuidv4 } from "uuid";
+import { execa } from "execa";
+import { tmpdir } from "os";
 
 export default async function main() {
-    await closeMainWindow();
+  await closeMainWindow();
 
-    const browser = await puppeteer.launch({headless: true});
-    const page = await browser.newPage();
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
 
-    try {
-        await page.setViewport({width: 1280, height: 800});
-        await page.goto('https://www.nytimes.com/');
-        const downloadPath = tmpdir() + "/" + uuidv4() + ".png";
-        await page.screenshot({path: downloadPath, fullPage: true, type: "png"});
+  try {
+    await page.setViewport({ width: 1280, height: 800 });
+    await page.goto("https://www.nytimes.com/");
+    const downloadPath = tmpdir() + "/" + uuidv4() + ".png";
+    await page.screenshot({ path: downloadPath, fullPage: true, type: "png" });
 
-        // with Execa
-        // await execa(
-        //     `osascript -e 'tell app "Finder" to set the clipboard to ( POSIX file "${downloadPath}" )'`,
-        //     [],
-        //     {shell: true}
-        // );
+    // with Execa
+    // await execa(
+    //     `osascript -e 'tell app "Finder" to set the clipboard to ( POSIX file "${downloadPath}" )'`,
+    //     [],
+    //     {shell: true}
+    // );
 
-        // with Raycast clipboard
-        await Clipboard.copy({file: downloadPath});
+    // with Raycast clipboard
+    await Clipboard.copy({ file: downloadPath });
 
-        await showHUD('Screenshot copied to your clipboard.');
-    } catch (err) {
-        await showHUD('Failed to get a screenshot.');
-    }
+    await showHUD("Screenshot copied to your clipboard.");
+  } catch (err) {
+    await showHUD("Failed to get a screenshot.");
+  }
 
-    await browser.close();
+  await browser.close();
 }
